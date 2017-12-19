609c9c7c8c4cb27f587a9996da523f33391d9f1b
kylepeng93 <kylepeng93@gmail.com>
2017-12-19 23:04:57 +0800

    添加了ngx-bootstrap-modal模块，并在index.html中添加了bootstrap3.3.7的link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>ngx-bootstrap-modal</h1>
+    <p>simplify the work with bootstrap modal dialogs</p>
+    <demo></demo>
+  `,
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'app';
 }
